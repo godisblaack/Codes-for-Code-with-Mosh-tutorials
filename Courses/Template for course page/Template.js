6618e54671a060a022503470872644eab84edc16
godisblaack <163236412+godisblaack@users.noreply.github.com>
2024-07-02 13:19:24 +0530

    Added template files

---
+++ Courses/Template for course page/Template.js
@@ -0,0 +1,35 @@
+function showCode(video) {
+  const codeContainer = document.getElementById('codeContainer');
+  const codeBlock = document.getElementById('code-block');
+  let code = '';
+
+  switch(video) {
+    case 'video1':
+      code = `
+// Code will be displayed here
+`;
+      break;
+    case 'videoNumber':
+      code = `
+Your code goes here.
+`;
+      break;
+    // Add more cases here for other videos
+    default:
+      code = 'Please select a video to view the code.';
+  }
+
+  codeBlock.textContent = code.trim();
+  hljs.highlightAll();
+}
+
+function copyCode() {
+  const codeBlock = document.getElementById('code-block');
+  const tempInput = document.createElement('textarea');
+  tempInput.value = codeBlock.textContent;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand('copy');
+  document.body.removeChild(tempInput);
+  alert('Code copied to clipboard!');
+}
